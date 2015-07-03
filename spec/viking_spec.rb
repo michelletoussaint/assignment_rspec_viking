43f9032e2360bdb1d7d6dca34e0ebeceedf57398
David <davidmeza@outlook.com>
2015-07-03 14:33:02 -0400

    add tests to viking spec file

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -36,7 +36,7 @@ describe Viking do
 
   describe "#pick_up_weapon" do
 
-    it 'overrides the current weapon' do
+    it 'picks up a weapon' do
 
       bow = Bow.new
       expect(oleg.weapon).to be nil
@@ -49,6 +49,61 @@ describe Viking do
     specify 'picking up a non-weapon raises an exception' do
 
       expect{oleg.pick_up_weapon("bow")}.to raise_error("Can't pick up that thing")
+
+    end
+
+    it 'overwrites current weapon' do
+
+      viking_4 = Viking.new("Sven", 120, 11, Bow.new)
+      my_axe = Axe.new
+      viking_4.pick_up_weapon(my_axe)
+      expect(viking_4.weapon).to eq(my_axe)
+
+    end
+
+  end
+
+  describe "#drop_weapon" do
+
+    it "leaves the viking weaponless" do
+
+      viking_4 = Viking.new("Sven", 120, 11, Bow.new)
+      viking_4.drop_weapon
+      expect(viking_4.weapon).to be nil
+
+    end
+
+  end
+
+  describe "#receive attack" do
+
+    it "reduces the viking's heatlh" do
+
+      oleg.receive_attack(10)
+
+      expect(oleg.health).to eq(90)
+
+    end
+
+    it "calls the #take_damage method" do
+
+      expect(oleg).to receive(:take_damage)
+
+      oleg.receive_attack(10)
+
+    end
+
+  end
+
+  describe "#attack" do
+
+    it "causes recipient's health to drop" do
+
+      sven = Viking.new("Sven")
+
+      oleg.attack(sven)
+
+      expect(sven.health).to eq(97.5)
 
     end
 
